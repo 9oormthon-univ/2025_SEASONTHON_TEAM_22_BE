4f9af28bd840e7ed225f360a9e58e3e4dfc6f8ba
Seungwon-Choi <bestwondev@gmail.com>
2025-08-30 17:09:34 +0900

    chore: Liquibase 연동 설정 추가 #3

---
+++ src/main/java/goormthonuniv/team_22_be/common/config/LiquibaseIncludeAllFilter.java
@@ -0,0 +1,13 @@
+package goormthonuniv.team_22_be.common.config;
+
+import liquibase.changelog.IncludeAllFilter;
+
+public class LiquibaseIncludeAllFilter implements IncludeAllFilter {
+
+    private static final String SQL_FORMAT = ".sql";
+
+    @Override
+    public boolean include(String changeLogPath) {
+        return changeLogPath.endsWith(SQL_FORMAT);
+    }
+}
