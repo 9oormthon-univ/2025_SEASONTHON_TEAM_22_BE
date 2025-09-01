8c329aa3c6bee777b1858c39a528a2f9afe6431d
Seungwon-Choi <bestwondev@gmail.com>
2025-09-02 02:06:52 +0900

    feat: activity_likes 테이블 추가 #10

---
+++ src/main/resources/liquibase/changelog/V005_create_table_activity_likes.sql
@@ -0,0 +1,17 @@
+-- liquibase formatted sql
+
+-- changeset Seungwon-Choi:5 runInTransaction:false
+
+CREATE TABLE IF NOT EXISTS activity_likes (
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY   NOT NULL COMMENT '활동 신청 PK',
+    member_id   bigint                              NOT NULL COMMENT '회원 FK',
+    activity_id bigint                              NOT NULL COMMENT '활동 FK',
+
+    CONSTRAINT fk_activity_likes_to_members
+        FOREIGN KEY (member_id) REFERENCES members (id),
+
+    CONSTRAINT fk_activity_likes_to_activities
+        FOREIGN KEY (activity_id) REFERENCES activities (id)
+);
+
+CREATE INDEX idx_activity_likes_member_id ON activity_likes(member_id);
