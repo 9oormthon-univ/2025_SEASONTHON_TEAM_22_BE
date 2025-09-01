5fb27fae54730dde44846013269f659fe0070ad7
Seungwon-Choi <bestwondev@gmail.com>
2025-09-02 02:51:51 +0900

    feat: EmotionRecord 엔티티 클래스 추가 #15

---
+++ src/main/java/goormthonuniv/team_22_be/emotion/domain/model/EmotionState.java
@@ -0,0 +1,15 @@
+package goormthonuniv.team_22_be.emotion.domain.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum EmotionState {
+
+    HAPPY,
+    SOSO,
+    BAD,
+    ANGER,
+    WORRY
+}
