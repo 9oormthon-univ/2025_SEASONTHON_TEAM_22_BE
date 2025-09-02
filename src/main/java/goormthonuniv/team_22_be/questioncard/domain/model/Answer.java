da82423eb32de45fe44b321fb3a29eee93aec36a
Seungwon-Choi <bestwondev@gmail.com>
2025-09-02 11:15:15 +0900

    feat: Answer 엔티티 클래스 추가 #15

---
+++ src/main/java/goormthonuniv/team_22_be/questioncard/domain/model/Answer.java
@@ -0,0 +1,53 @@
+package goormthonuniv.team_22_be.questioncard.domain.model;
+
+import goormthonuniv.team_22_be.common.utils.BaseTimeEntity;
+import goormthonuniv.team_22_be.member.domain.model.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "answers")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+public class Answer extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_card_id")
+    private QuestionCard questionCard;
+
+    private String content;
+
+    // 정적 팩토리 메서드
+    public static Answer create(Member member, QuestionCard questionCard, String content) {
+        return Answer.builder()
+                .member(member)
+                .questionCard(questionCard)
+                .content(content)
+                .build();
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+}
