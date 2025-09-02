c0292756fec80046dce1056260a93d4132d80f07
Seungwon-Choi <bestwondev@gmail.com>
2025-09-02 11:10:46 +0900

    feat: QuestionCard 엔티티 클래스 추가 #15

---
+++ src/main/java/goormthonuniv/team_22_be/questioncard/domain/model/QuestionCard.java
@@ -0,0 +1,39 @@
+package goormthonuniv.team_22_be.questioncard.domain.model;
+
+import goormthonuniv.team_22_be.common.utils.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "question_cards")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+public class QuestionCard extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    // 정적 팩토리 메서드
+    public static QuestionCard create(String content) {
+        return QuestionCard.builder()
+                .content(content)
+                .build();
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+}
