e9b71f0d4930a70f439633c54ade4d661be9ee8e
newrhyme <ksh000624@naver.com>
2025-09-04 16:57:05 +0900

    chore : VAPID 키 설정 관련 gradle 의존성 및 VapidKeysConfig 추가

---
+++ src/main/java/goormthonuniv/team_22_be/common/config/VapidKeysConfig.java
@@ -0,0 +1,16 @@
+package goormthonuniv.team_22_be.common.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "webpush.vapid")
+public class VapidKeysConfig {
+    private String publicKey;
+    private String privateKey;
+    private String subject;
+}
