9e2507e5f93c3aba37ebed866d0f3cf1a08811a1
Seungwon-Choi <bestwondev@gmail.com>
2025-09-05 15:55:27 +0900

    test: Comment 테스트 코드 수정

--- src/test/java/goormthonuniv/team_22_be/post/domain/model/CommentTest.java
+++ src/test/java/goormthonuniv/team_22_be/post/domain/model/CommentTest.java
@@ -1,5 +1,6 @@
 package goormthonuniv.team_22_be.post.domain.model;
 
+import goormthonuniv.team_22_be.comment.domain.model.Comment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,15 +12,13 @@ class CommentTest {
     @DisplayName("댓글 생성 테스트")
     void createCommentTest() {
         // given
-        String accountId = "user123";
         String content = "테스트 댓글";
 
         // when
-        Comment comment = Comment.create(null, accountId, content);
+        Comment comment = Comment.create(null, null, content);
 
         // then
         assertNotNull(comment);
-        assertEquals(accountId, comment.getAccountId());
         assertEquals(content, comment.getContent());
     }
 
@@ -29,7 +28,7 @@ class CommentTest {
         // given
         String accountId = "user123";
         String content = "테스트 댓글";
-        Comment comment = Comment.create(null, accountId, content);
+        Comment comment = Comment.create(null, null, content);
 
         String newContent = "수정 댓글";
 
