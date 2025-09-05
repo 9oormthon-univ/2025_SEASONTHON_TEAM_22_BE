5b47c03585fbfdd0f74fced4b3fc5fdf9921b3d7
newrhyme <ksh000624@naver.com>
2025-09-05 17:43:51 +0900

    feat : PasswordEncoder Bean 추가

--- src/main/java/goormthonuniv/team_22_be/common/config/SecurityConfig.java
+++ src/main/java/goormthonuniv/team_22_be/common/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,6 +23,11 @@ public class SecurityConfig {
     public SecurityConfig(JwtAuthFilter jwtAuthFilter, AuthenticationSuccessHandler successHandler) {
         this.jwtAuthFilter = jwtAuthFilter;
         this.successHandler = successHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
