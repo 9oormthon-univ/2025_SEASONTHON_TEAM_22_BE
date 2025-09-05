c297ba8763e4647ecb092b56ad49d5ea6ea2438c
newrhyme <ksh000624@naver.com>
2025-09-06 05:53:35 +0900

    chore : firebase로 변경;;

--- src/main/java/goormthonuniv/team_22_be/notification/application/dto/SchduledActivityDadlineRequestDto.java
+++
@@ -1,4 +0,0 @@
-package goormthonuniv.team_22_be.notification.application.dto;
-
-public record SchduledActivityDadlineRequestDto() {
-}
--- src/main/java/goormthonuniv/team_22_be/notification/domain/model/NotificationType.java
+++
@@ -1,7 +0,0 @@
-package goormthonuniv.team_22_be.notification.domain.model;
-
-public enum NotificationType {
-    LIKE_DEADLINE,
-    ACTIVITY_DEADLINE_D1,
-    ACTIVITY_DEADLINE_H1
-}
